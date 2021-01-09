0ce52f7114fa9cd8d307cb089bb0a7c17c54c5b4
DonadiHarshith <71581388+DonadiHarshith@users.noreply.github.com>
2021-01-09 16:36:13 +0530

    Add files via upload

---
+++ main_main.js
@@ -0,0 +1,19 @@
+function bedroom() {
+    window.location="bedroom_index.html"
+}
+
+function living_room() {
+    window.location="living_room.html"
+}
+
+function desk() {
+    window.location="desk_index.html"
+}
+
+function kitchen() {
+    window.location="kitchen_index.html"
+}
+
+function shelves() {
+    window.location="shelves_index.html"
+}
